fa0def70d6cee5ad63afe05a9238abb71352bdcb
Masato Yamashita <masatoyamashita@rrcs-172-254-99-63.nyc.biz.rr.com>
2019-03-12 01:36:42 +0900

    Otoshidama program added.

--- Otoshidama/Program.cs
+++ Otoshidama/Program.cs
@@ -6,7 +6,48 @@ namespace Otoshidama
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string input = Console.ReadLine();
+            string[] ny = input.Split(' ');
+            int n = Int32.Parse(ny[0]);
+            int y = Int32.Parse(ny[1]);
+
+            bool isOk = equalN(n, y);
+
+            if (!isOk)
+            {
+                Console.WriteLine("-1 -1 -1");
+            }
+            
+        }
+
+        private static bool equalN(int n, int y)
+        {
+            for (int i = 0; i <= n; i++)
+            {
+                if (10000 * i > y)
+                {
+                    return false;
+                }
+                for (int j = 0; j <= n - i; j++)                
+                {
+                    if (10000 * i + 5000 * j > y)
+                    {
+                        continue;
+                    }
+
+                    int k = n - i - j;
+
+                    if (10000 * i + 5000 * j + 1000 * k == y)
+                    {
+
+                        Console.WriteLine(i + " " + j + " " + k);
+                        return true;
+                    }
+
+                }
+            }
+
+            return false;
         }
     }
 }
